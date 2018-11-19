668294747aa5b32e2b4f9876dd85edb6246a137d
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2018-11-19 16:50:12 +0530

    Update demo5.py

--- demo5.py
+++ demo5.py
@@ -4,8 +4,8 @@ str = "hat rat mat pat"
 
 res = re.compile('[r]at')
 
-print(res)          #  re.compile('[r]at')
-print(type(res))    #  <class '_sre.SRE_Pattern'>
+print(res)          #  output is ---> re.compile('[r]at')
+print(type(res))    #  output is ---> <class '_sre.SRE_Pattern'>
 
 alt = res.sub("alter",str)
 print(alt)
